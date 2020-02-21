0a73865801b4e84aa1a3389a0bd24763cbcbd30c
slikts <dabas@untu.ms>
2020-02-21 16:59:28 +0000

    Updates

---
+++ precache-manifest.4cd59e004d963618796e66513adc5b9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f51ca077ab47ea8355e33a97f3cb6c85",
+    "url": "/catsup/index.html"
+  },
+  {
+    "revision": "db4e2d722c6c3b794e9f",
+    "url": "/catsup/static/css/main.102efdd9.chunk.css"
+  },
+  {
+    "revision": "08a165aec338545f8b36",
+    "url": "/catsup/static/js/2.40d2c193.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/catsup/static/js/2.40d2c193.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db4e2d722c6c3b794e9f",
+    "url": "/catsup/static/js/main.44f1f684.chunk.js"
+  },
+  {
+    "revision": "fa8a62668bd6af7a787e",
+    "url": "/catsup/static/js/runtime-main.47df48e8.js"
+  },
+  {
+    "revision": "a13ba14b89d4068797627efc42d99e45",
+    "url": "/catsup/static/media/letters.a13ba14b.svg"
+  }
+]);
